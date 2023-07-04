b809a166277f29f8c3ad018936f473711f237b04
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-07-04 14:44:59 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+
+const port = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+  res.send("hello woeld!")  
+})
+
+app.listen(port, () => console.log(`listening on port ${port}!`))
