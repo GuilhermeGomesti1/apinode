226909cf1a797504b2ffa23e2e5eba1bbab5c531
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-07-05 19:50:28 -0300

    getUsersMongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,16 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
 import { User } from "../../models/user";
-
+import { MongoClient } from "../../database/mongo";
 export class MongoGetUsersRepository implements IGetUsersRepository {
-   async getUsers(): Promise<User[]> {
-        return [{
-            firstName: 'Felipe',
-            lastName: "Rocha",
-            email: "felipe@gmail.com",
-            password:"123",
-        }]
-    }
+  async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
+  }
 }
-
-
