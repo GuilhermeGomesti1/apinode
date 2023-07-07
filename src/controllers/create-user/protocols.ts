20645704da3c80d4e5380f7e01fad35ada9f7ff6
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-07-07 20:57:10 -0300

    refactor: add IController common interface

--- src/controllers/create-user/protocols.ts
+++ src/controllers/create-user/protocols.ts
@@ -1,8 +1,7 @@
 import { User } from "../../models/user";
-import { HttpRequest, HttpResponse } from "../protocols";
-export interface ICreateUserController{
-    handle(httpResquest: HttpRequest<CreateUserParams>): Promise<HttpResponse<User>>;
-}
+
+
+   
 
 export interface CreateUserParams{
     firstName: string;
--- src/controllers/get-users/protocols.ts
+++ src/controllers/get-users/protocols.ts
@@ -1,8 +1,5 @@
 import { User } from "../../models/user";
-import { HttpResponse } from "../protocols";
-export interface IGetUsersController{
-    handle(): Promise<HttpResponse<User[]>>;
-}
+
 
 export interface IGetUsersRepository{
     getUsers(): Promise<User[]>;
